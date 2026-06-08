d4166530f208081d9074eb7270a4902e2f4897ff
Sona Binu <sonavbinu567@gmail.com>
2026-06-08 16:57:57 +0530

    add

--- react/src/Components/employee/EmployeeProfile.jsx
+++ react/src/Components/employee/EmployeeProfile.jsx
@@ -27,15 +27,17 @@ const EmployeeProfile = () => {
   }
   return (
     <Card className="flex items-center justify-center max-w-7xl">
-      <div className="border border-gray-100 shadow-xl p-10 rounded-xl w-full bg-white ">
-        <div className="flex gap-4 items-center bg-blue-50 border-blue-200 border p-3 rounded-3xl">
+      <div className="border border-gray-100 shadow-xl p-10 rounded-xl w-full bg-white dark:bg-slate-900 ">
+        <div className="flex gap-4 items-center dark:bg-gray-500 bg-blue-50 border-blue-200 border p-3 rounded-3xl">
           <div className="border rounded-4xl w-[24px] px-5 py-2 flex justify-center bg-slate-800 text-white font-bold ">
             {employee.name?.charAt(0)}
           </div>
           <div>
             <div className=" rounded-3xl w-full flex flex-col items-center">
               <h1 className="text-xl font-bold ">{employee.name}</h1>
-              <p className="text-gray-500">{employee.position}</p>
+              <p className="text-gray-500  dark:text-white">
+                {employee.position}
+              </p>
             </div>
           </div>
         </div>
