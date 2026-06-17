ddbe85d578e6ef3fc07e0cfed3363659d2d41092
Sona Binu <sonavbinu567@gmail.com>
2026-06-17 22:10:27 +0530

    add

--- react/src/features/master/City.jsx/components/CityFormModal.jsx
+++ react/src/features/master/City.jsx/components/CityFormModal.jsx
@@ -4,92 +4,104 @@ const CityFormModal = ({
   name,
   code,
   setName,
-  edit,
   setCode,
+  edit,
   onSave,
   onClose,
-  states = [],
-  stateId,
-  setStateId,
-  countryId,
-  setCountryId,
+
   countries = [],
+  countryId = "",
+  setCountryId,
+
+  states = [],
+  stateId = "",
+  setStateId,
 }) => {
   return (
-    <div>
-      <div className="flex flex-col justify-center items-center gap-2 fixed inset-0 bg-black/30 backdrop-blur-sm z-50">
-        <div className="border border-gray-300 p-2 bg-white rounded-lg shadow-2xl p-6 ">
-          <div className="p-2">
-            <div>
-              <h2 className="text-xl font-bold mb-4">
-                {edit ? "Edit City" : "Add City"}
-              </h2>
+    <div className="fixed inset-0 bg-black/30 backdrop-blur-sm z-50 flex justify-center items-center">
+      <div className="bg-white rounded-lg shadow-2xl p-6 w-[450px]">
+        <h2 className="text-xl font-bold mb-4">
+          {edit ? "Edit City" : "Add City"}
+        </h2>
 
-              <div className="flex  flex-col">
-                <label>Country</label>
-                <select
-                  value={countryId}
-                  onChange={(e) => setCountryId(e.target.value)}
-                  className="border p-2 rounded"
-                >
-                  <option value="">Select Country</option>
+        {/* Country */}
+        <div className="mb-4">
+          <label className="block mb-1 font-medium">Country</label>
 
-                  {countries.map((country) => (
-                    <option value={country._id} key={country._id}>
-                      {country.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="mb-4">
-                <label className="block mb-1 font-medium">State</label>
-                <select
-                  value={stateId}
-                  onChange={(e) => setStateId(e.target.value)}
-                  className="w-full border p-2 rounded"
-                >
-                  <option value="">Select State</option>
+          <select
+            value={countryId}
+            onChange={(e) => setCountryId(e.target.value)}
+            className="w-full border p-2 rounded"
+          >
+            <option value="">Select Country</option>
 
-                  {states.map((state) => (
-                    <option value={state._id} key={states._id}>
-                      {state.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-            <label>City Name</label>
-            <input
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-              className="w-full h-[40px] border px-2 "
-            />
-          </div>
-          <div className="p-2">
-            <label>City Code</label>
-            <input
-              type="text"
-              onChange={(e) => setCode(e.target.value.toUpperCase())}
-              value={code}
-              className="w-full h-[40px] border px-2"
-            />
-          </div>
-          <div className="flex justify-around">
-            <button
-              className="px-4 py-2 bg-gray-300 rounded cursor-pointer hover:bg-gray-500 hover:text-white"
-              onClick={onClose}
-            >
-              Cancel
-            </button>
+            {countries.map((country) => (
+              <option key={country._id} value={country._id}>
+                {country.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-            <button
-              onClick={onSave}
-              className="bg-gray-500 text-white px-4 py-2 hover:bg-gray-300 cursor-pointer shadow-xl hover:shadow-3xl rounded-lg hover:text-black"
-            >
-              {edit ? "Update" : "Add"}
-            </button>
-          </div>
+        {/* State */}
+        <div className="mb-4">
+          <label className="block mb-1 font-medium">State</label>
+
+          <select
+            value={stateId}
+            onChange={(e) => setStateId(e.target.value)}
+            className="w-full border p-2 rounded"
+          >
+            <option value="">Select State</option>
+
+            {states.map((state) => (
+              <option key={state._id} value={state._id}>
+                {state.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        {/* City Name */}
+        <div className="mb-4">
+          <label className="block mb-1 font-medium">City Name</label>
+
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full border p-2 rounded"
+            placeholder="Enter city name"
+          />
+        </div>
+
+        {/* City Code */}
+        <div className="mb-6">
+          <label className="block mb-1 font-medium">City Code</label>
+
+          <input
+            type="text"
+            value={code}
+            onChange={(e) => setCode(e.target.value.toUpperCase())}
+            className="w-full border p-2 rounded"
+            placeholder="Enter city code"
+          />
+        </div>
+
+        <div className="flex justify-end gap-3">
+          <button
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
+          >
+            Cancel
+          </button>
+
+          <button
+            onClick={onSave}
+            className="px-4 py-2 bg-slate-800 text-white rounded hover:bg-slate-700"
+          >
+            {edit ? "Update" : "Add"}
+          </button>
         </div>
       </div>
     </div>
--- react/src/features/master/City.jsx/pages/City.jsx
+++ react/src/features/master/City.jsx/pages/City.jsx
@@ -24,15 +24,12 @@ const City = () => {
   const [countries, setCountries] = useState([]);
   const [countryId, setCountryId] = useState("");
 
-  const [filterCountry, setFilterCountry] = useState("");
-  const [filterState, setFilterState] = useState("");
-  const [filterCity, setFilterCity] = useState("");
-
-  const [filterStates, setFilterStates] = useState([]);
-  const [filterCities, setFilterCities] = useState([]);
+  const [filterName, setFilterName] = useState("");
 
   useEffect(() => {
     fetchCountries();
+    fetchCity();
+    fetchStates();
   }, []);
 
   const fetchCountries = async () => {
@@ -40,13 +37,9 @@ const City = () => {
       const res = await api.get("/country");
       setCountries(res.data);
     } catch (error) {
-      console.log(error);
+      console.log("Country fetch error", error);
     }
   };
-
-  useEffect(() => {
-    fetchStates();
-  }, []);
 
   const fetchStates = async () => {
     try {
@@ -58,44 +51,47 @@ const City = () => {
     }
   };
 
-  useEffect(() => {
-    fetchCity();
-  }, []);
+  const fetchCity = async () => {
+    try {
+      const { data } = await getCities();
+      setCity(data);
+    } catch (error) {
+      console.log("City fetch error:", error);
+    }
+  };
 
   const handleAdd = async () => {
     try {
-      if (edit) {
-        await updateCity(edit, { name, code, stateId });
-      } else {
-        await addCity({ name, code, stateId });
-      }
+      console.log({
+        name,
+        code,
+        stateId,
+      });
 
-      fetchCity();
+      const response = edit
+        ? await updateCity(edit, { name, code, stateId })
+        : await addCity({ name, code, stateId });
+
+      console.log("Response:", response);
+
+      await fetchCity();
       closeForm();
     } catch (error) {
-      console.log(error);
+      console.log("ADD ERROR:", error);
     }
   };
 
   const resetForm = () => {
     setName("");
     setCode("");
+    setStateId("");
+    setCountryId("");
     setEdit(null);
   };
 
   const closeForm = () => {
     resetForm();
     setShowForm(false);
-  };
-
-  const fetchCity = async () => {
-    try {
-      const { data } = await getCities();
-      console.log(data);
-      setCity(data);
-    } catch (error) {
-      console.log(error);
-    }
   };
 
   const handleEdit = (city) => {
@@ -151,36 +147,11 @@ const City = () => {
       console.log(error);
     }
   };
-  const handleFilterCountry = async (id) => {
-    setFilterCountry(id);
-    setFilterState("");
 
-    try {
-      const res = await api.get(`/state/country/${id}`);
-      setFilterStates(res.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  const filterCities = city.filter((cities) =>
+    cities?.name?.toLowerCase().includes(filterName.toLowerCase()),
+  );
 
-  const handleFilterState = (id) => {
-    setFilterState(id);
-  };
-
-  const handleFilterCity = async (id) => {
-    setFilterCountry(id);
-    setFilterState(id);
-    setFilterCity("");
-
-    const res = await api.get(`/city/state/${id}`);
-    setFilterCities(res.data);
-  };
-  const filteredCities = city.filter((item) => {
-    if (filterState && item.stateId?._id !== filterState) {
-      return false;
-    }
-    return true;
-  });
   return (
     <div>
       <div className="flex justify-between items-center px-2">
@@ -218,34 +189,14 @@ const City = () => {
       <div className="flex justify-center items-center p-2 flex-col items-center mt-4 gap-10">
         <div className="flex w-full">
           <div className="w-full">
-            <div className="flex gap-3 mb-4">
-              <select
-                value={filterCountry}
-                onChange={(e) => handleFilterCountry(e.target.value)}
-                className="border border-gray-300 p-2 rounded"
-              >
-                <option value="">All Countries</option>
-
-                {countries.map((country) => (
-                  <option key={country._id} value={country._id}>
-                    {country.name}
-                  </option>
-                ))}
-              </select>
-
-              <select
-                value={filterState}
-                onChange={(e) => handleFilterState(e.target.value)}
-                className="border border-gray-300 p-2 rounded"
-              >
-                <option value="">All States</option>
-
-                {filterStates.map((state) => (
-                  <option key={state._id} value={state._id}>
-                    {state.name}
-                  </option>
-                ))}
-              </select>
+            <div>
+              <input
+                type="text"
+                placeholder="Search City..."
+                value={filterName}
+                onChange={(e) => setFilterName(e.target.value)}
+                className="border border-gray-300 rounded p-2 w-64"
+              />
             </div>
             <table className=" bg-white w-full">
               <thead>
@@ -257,7 +208,7 @@ const City = () => {
                 </tr>
               </thead>
               <tbody>
-                {filteredCities.map((cities, index) => (
+                {filterCities.map((cities, index) => (
                   <tr key={cities._id} className="hover:bg-gray-50">
                     <td className="border p-2 border-gray-300">{index + 1}</td>
                     <td className="border p-2 border-gray-300">
